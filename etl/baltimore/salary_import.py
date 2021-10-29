17f6a41af1286efceaf4380540336d413c63cb48
Dan Staples <dan@acab.enterprises>
2021-10-29 18:15:23 -0400

    Update salary import script for FY2021 data

--- etl/baltimore/salary_import.py
+++ etl/baltimore/salary_import.py
@@ -1,23 +1,18 @@
 import pandas as pd
 import os
 import sys
-import glob
-import re
 import numpy
 import pdb
-from datetime import date
-from utils import name_re
+from utils import last_name_re
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
-from OpenOversight.app import create_app  # noqa E402
+from OpenOversight.app import app  # noqa E402
 from OpenOversight.app.models import db, Officer, Salary  # noqa E402
 from OpenOversight.app.utils import prompt_yes_no
 
-app = create_app('development')
 db.app = app
 
-# 2020-8-28 import
 missing_officers = [
     'F956',
     'T819',
@@ -31,7 +26,7 @@ missing_officers = [
 ]
 
 BEGIN_YEAR = 2011
-END_YEAR = 2020
+END_YEAR = 2021
 
 
 class SalaryImportLog:
@@ -57,20 +52,20 @@ class SalaryImportLog:
 def add_salary(officer, rows, year):
     row = rows.loc[rows.first_valid_index()]
 
-    if type(row['AnnualSalary']) == str:
-        annual_salary = float(row['AnnualSalary'].replace('$', ''))
-    elif type(row['AnnualSalary']) == float or type(row['AnnualSalary']) == numpy.float64:
-        annual_salary = row['AnnualSalary']
+    if type(row['annualSalary']) == str:
+        annual_salary = float(row['annualSalary'].replace('$', ''))
+    elif type(row['annualSalary']) == float or type(row['annualSalary']) == numpy.float64:
+        annual_salary = row['annualSalary']
     else:
-        raise Exception(type(row['AnnualSalary']))
+        raise Exception(type(row['annualSalary']))
 
-    if type(row['GrossPay']) == str:
-        overtime_pay = float(row['GrossPay'].replace('$', '')) - annual_salary
-    elif type(row['GrossPay']) == float or type(row['GrossPay']) == numpy.float64:
-        if numpy.isnan(row['GrossPay']):
+    if type(row['grossPay']) == str:
+        overtime_pay = float(row['grossPay'].replace('$', '')) - annual_salary
+    elif type(row['grossPay']) == float or type(row['grossPay']) == numpy.float64:
+        if numpy.isnan(row['grossPay']):
             overtime_pay = 0
         else:
-            overtime_pay = row['GrossPay'] - annual_salary
+            overtime_pay = row['grossPay'] - annual_salary
 
     salary = Salary(
         officer_id=officer.id,
@@ -86,32 +81,21 @@ def add_salary(officer, rows, year):
     )
 
 
-# Manual fixes to CSV data
-def clean_dataframe(df):
-    df.loc[df['ID'] == 137525, 'Name'] = 'Adams III,Wayne A'
-    df.loc[df['ID'] == 31336, 'Name'] = 'Banks Jr.,Joseph A'
-    df.loc[df['ID'] == 50450, 'Name'] = 'Banks Jr.,Joseph A'
-    df.loc[df['ID'] == 69223, 'Name'] = 'Banks Jr.,Joseph A'
-    df.loc[df['ID'] == 83241, 'Name'] = 'Banks Jr.,Joseph A'
-    return df
-
-
 def import_salaries(csv_filename):
     print("Processing salary charts")
     df = pd.read_csv(csv_filename)
-    df = clean_dataframe(df)
     # # Only want A99 department IDs
-    df = df[df.loc[:, 'AgencyID'].str.startswith('A99')]
+    df = df[df.loc[:, 'agencyID'].str.startswith('A99')]
     # Skip officers with redacted names
-    df = df[~df.loc[:, 'Name'].str.startswith('BPD ')]
-    # Split name into multiple columns
-    df = df.join(df.loc[:, 'Name'].str.extract(name_re))
+    df = df[~df.loc[:, 'lastName'].fillna('').str.startswith('BPD ')]
+    # Split last name into multiple columns
+    df = df.join(df.loc[:, 'lastName'].str.extract(last_name_re))
 
 
     print("Importing salaries (this may take a while)")
     try:
         # foreach officer in DB, go matching
-        for officer in Officer.query.order_by(Officer.id.asc()).all():
+        for officer in Officer.query.filter_by(department_id=1).order_by(Officer.id.asc()).all():
             added_salaries = False
             print(officer)
             officer_existing_salary_years = [salary.year for salary in officer.salaries]
@@ -120,10 +104,10 @@ def import_salaries(csv_filename):
                 # Don't check for salaries for years we already have data
                 if year in officer_existing_salary_years:
                     continue
-                year_df = df[df.loc[:, 'FiscalYear'].str.match('FY{}'.format(year))]
+                year_df = df[df.loc[:, 'fiscalYear'].str.match('FY{}'.format(year))]
                 # Match on first name, last name
-                rows = year_df[year_df['first_name'].fillna('').str.lower().str.match(officer.first_name.lower())]\
-                    [lambda year_df: year_df['last_name'].fillna('').str.lower().str.match(officer.last_name.lower())]
+                rows = year_df[year_df['firstName'].fillna('').str.lower().str.match(officer.first_name.lower())]\
+                    [lambda year_df: year_df['lastName'].fillna('').str.lower().str.match(officer.last_name.lower())]
                 if rows.empty:
                     continue
                 elif len(rows) == 1:
@@ -132,9 +116,9 @@ def import_salaries(csv_filename):
                 elif len(rows) > 1:
                     # Match on (first letter of) middle initial
                     if officer.middle_initial:
-                        rows = rows[rows['middle_initial'].fillna('').str.upper().str.startswith(officer.middle_initial[:1].upper())]
+                        rows = rows[rows['middleInitial'].fillna('').str.upper().str.startswith(officer.middle_initial[:1].upper())]
                     else:
-                        rows = rows[rows['middle_initial'].isna()]
+                        rows = rows[rows['middleInitial'].fillna('').str.match('NA')]
                     if len(rows) == 1:
                         added_salaries = True
                         add_salary(officer, rows, year)
@@ -161,20 +145,20 @@ def import_salaries(csv_filename):
                         if len(rows) == 1:
                             added_salaries = True
                             add_salary(officer, rows, year)
-                        else:
+                        elif len(rows) > 1:
                             # Match on hiring date
                             hire_date = officer.employment_date.strftime('%m/%d/%Y')
-                            rows = rows[rows.loc[:, 'HireDate'].str.startswith(hire_date)]
+                            rows = rows[rows.loc[:, 'hireDate'].str.startswith(hire_date)]
                             if len(rows) == 1:
                                 added_salaries = True
                                 add_salary(officer, rows, year)
-                            else:
+                            elif len(rows) > 1:
                                 print('Could not match on suffix or hire date for {} {} {} {}'.format(
                                         officer.first_name, officer.middle_initial,
                                         officer.last_name, officer.suffix))
                                 pdb.set_trace()
             if not added_salaries and len(officer.salaries) == 0:
-                if officer.employment_date < date(year=2019, month=1, day=1) and officer.unique_internal_identifier not in missing_officers:
+                if officer.unique_internal_identifier not in missing_officers:
                     print(f'Officer {officer.last_name},{officer.first_name} ({officer.unique_internal_identifier}) not found in salary charts')
                     pdb.set_trace()
         
--- etl/baltimore/utils.py
+++ etl/baltimore/utils.py
@@ -18,6 +18,7 @@ suffixes = [
 ]
 
 name_re = re.compile(r"^(?P<last_name>[a-zA-Z- '\.]+?)(?: (?P<suffix>(?i:" + r"|".join(suffixes) + r"))\.?)?,(?P<first_name>[a-zA-Z- '\.]+?)(?: (?P<middle_initial>[a-zA-Z])\.?)?$")
+last_name_re = re.compile(r"^(?P<last_name>[a-zA-Z- '\.]+?)(?: (?P<suffix>(?i:" + r"|".join(suffixes) + r"))\.?)?$")
 
 
 def eprint(*args, **kwargs):
